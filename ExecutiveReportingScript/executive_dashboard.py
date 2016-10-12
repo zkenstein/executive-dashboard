1b6cbc4893f5a37926ff09d5ae7c94b009540f0d
Allison Muise <amuise@esri.com>
2016-10-12 15:17:49 -0700

    added copyright text to scripts

--- ExecutiveReportingScript/executive_dashboard.py
+++ ExecutiveReportingScript/executive_dashboard.py
@@ -6,7 +6,22 @@
 #
 # Created:      05/06/2016 AM
 # Version:      Python 2.7
-#-------------------------------------------------------------------------------
+
+# Copyright 2016 Esri
+
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+# ------------------------------------------------------------------------------
 
 import json, urllib, arcrest, re
 from arcrest.security import AGOLTokenSecurityHandler
@@ -289,7 +304,7 @@ def main():
             if min_date is None:
                 attributes[ic.start_date] = stats[ic.end_date]
             else:
-                attributes[ic.start_date] = get_epoch_time(min_date)        
+                attributes[ic.start_date] = get_epoch_time(min_date)
 
             edits = [{"attributes" : attributes}]
             statslayer.applyEdits(updateFeatures=edits)
